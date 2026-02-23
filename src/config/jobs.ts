bc0f689fb1834c38a74d16532eb1d79d68431f34
Ryan Thurbon <ryanthurbon3@gmail.com>
2026-02-23 21:44:03 +0200

    modify about

--- src/config/jobs.ts
+++ src/config/jobs.ts
@@ -1,7 +1,8 @@
-import { WithLogo, WithTimePeriod, WithTitle, WithURL } from "@/config/types"
+import { WithTimePeriod, WithTitle, WithURL } from "@/config/types"
 
-export interface JobItem extends WithTitle, WithTimePeriod, WithURL, WithLogo {
+export interface JobItem extends WithTitle, WithTimePeriod, WithURL {
     role: string
+    description: string
 }
 
 export const jobs: JobItem[] = []
--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -1,13 +1,13 @@
-import { SectionHeading } from "@/components/shared/section-heading.tsx"
 import { AnimatedSection } from "@/components/shared/animated-section.tsx"
 import { Footer } from "@/components/shared/footer.tsx"
+import { SectionHeading } from "@/components/shared/section-heading.tsx"
 
 const sections = [
-    "The coding journey started back in high school with Delphi, sparking a lasting love for learning and building. Free time often means exploring new languages and frameworks — these days, it’s mostly React and modern web tech. Every dive into something new adds to the excitement of creating software that matters.",
+    "The coding journey started back in high school with Delphi, sparking a lasting love for learning and building. Free time often means exploring new languages and frameworks — these days, it’s mostly React and modern web tech.",
 
-    "I build tools that make life easier, approaching every project with curiosity and persistence. Every line of code is an opportunity to learn, and every project—finished or in progress—adds valuable experience. I focus on clean, maintainable systems and simple, effective solutions, where usability comes first for both developers and users.",
+    "I build tools that make life easier, approaching every project with curiosity and persistence.I focus on clean, maintainable systems and simple, effective solutions, where usability comes first for both developers and users.",
 
-    "Tackling challenges that require creative problem-solving and careful thinking is where the work gets exciting. Finding elegant, efficient solutions is as satisfying as seeing a project come to life.",
+    "Tackling challenges that require creative problem-solving and careful thinking is where the work gets exciting.",
 
     "Collaboration and communication are key to building great software. I value sharing ideas, iterating with feedback, and contributing to projects that have a real impact.",
 ]
